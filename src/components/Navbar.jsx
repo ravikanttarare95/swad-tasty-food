da367b1a0a7a4321ed462b8701fc9ce9cd4f6e63
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-08-06 10:05:39 +0530

    Added Logc to Nav Menu Icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./../../public/logo.png";
 import { NAV_LINKS } from "./../configs/Navbar";
 import { Link } from "react-router";
-import { ShoppingBag, Menu } from "lucide-react";
+import { ShoppingBag, Menu, X } from "lucide-react";
 
 function Navbar() {
+  const [isHidden, setIsHidden] = useState(true);
   return (
     <nav className="bg-white shadow-md text-gray-800">
       <div className="px-4 py-3 flex flex-col md:flex-row items-center justify-between">
@@ -14,22 +15,43 @@ function Navbar() {
             <img src={logo} alt="Logo" className="w-12 h-12 object-contain" />
             <Link
               to="/"
-              className="text-3xl font-extrabold text-green-700 hover:text-green-900 transition"
+              className="text-3xl font-extrabold text-green-500 hover:text-green-900 transition"
             >
               Swad
             </Link>
           </div>
 
-          <Menu size={30} className="md:hidden" />
+          <X
+            size={30}
+            className={` ${isHidden ? "hidden" : "block"} md:hidden`}
+            onClick={() => {
+              setIsHidden(!isHidden);
+            }}
+          />
+
+          <Menu
+            size={30}
+            className={` ${isHidden ? "block" : "hidden"} md:hidden`}
+            onClick={() => {
+              setIsHidden(!isHidden);
+            }}
+          />
         </div>
         {/* Navigation Links */}
-        <div className="w-full md:w-[80%] flex  flex-col items-center gap-3 md:flex-row text-center border md:border-0 rounded pt-3 pb-5 mt-2">
+        <div
+          className={` ${
+            isHidden ? "hidden" : "flex"
+          } w-full md:w-[80%] md:flex flex-col items-center gap-3 md:flex-row text-center bg-gray-200 md:bg-transparent rounded pt-3 pb-5 mt-2 md:pt-0 md:pb-0 md:mt-0`}
+        >
           <ul className=" space-y-3 md:space-y-0  md:flex md:ml-auto md:space-x-5">
             {NAV_LINKS.map((link) => (
               <li key={link.to}>
                 <Link
                   to={link.to}
                   className="sm:text-lg font-medium text-gray-700 hover:text-amber-600 transition"
+                  onClick={() => {
+                    setIsHidden(!isHidden);
+                  }}
                 >
                   {link.navlink}
                 </Link>
@@ -41,14 +63,14 @@ function Navbar() {
           <div className="space-y-5 md:space-y-0 flex flex-col md:flex-row md:space-x-5 items-center md:ml-auto">
             <Link
               to="/cart"
-              className="flex items-center space-x-1 text-red-600 hover:text-red-800 font-medium transition"
+              className="flex items-center space-x-1 text-red-500 hover:text-red-600 font-medium transition"
             >
               <span>Cart</span>
               <ShoppingBag className="w-5 h-5" />
             </Link>
             <Link
               to="/sign_in"
-              className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-lg text-sm font-semibold transition"
+              className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-sm font-semibold transition"
             >
               Sign In
             </Link>
