c221edf46a25c9be314941d8e89208133c966981
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-08-12 19:46:49 +0530

    .

--- src/components/cards/AboutCard.jsx
+++ src/components/cards/AboutCard.jsx
@@ -13,12 +13,18 @@ function AboutCard({ image, name, role, desc }) {
       <h3 className="text-2xl font-semibold text-gray-800">{name}</h3>
       <p className="text-sm font-semibold text-green-700 mb-3">{role}</p>
 
-      <p
-        className="flex absolute top-0 inset-0 opacity-0 group-hover:opacity-100 group-focus:opacity-100 group-focus:scale-100  scale-50 group-hover:scale-100 group-active:opacity-100 group-active:scale-100 transition-all duration-300 items-center justify-center bg-gradient-to-b from-green-700/80 via-green-800/90 to-gray-950
- backdrop-blur-[3px] mx-auto px-5 text-white text-md leading-relaxed rounded-2xl"
-      >
-        {desc}
-      </p>
+       <p
+    className="flex absolute top-0 inset-0 opacity-0 scale-50
+      group-hover:opacity-100 group-hover:scale-100
+      group-focus:opacity-100 group-focus:scale-100
+      group-active:opacity-100 group-active:scale-100
+      focus-within:opacity-100 focus-within:scale-100
+      transition-all duration-300 items-center justify-center
+      bg-gradient-to-b from-green-700/80 via-green-800/90 to-gray-950
+      backdrop-blur-[3px] mx-auto px-5 text-white text-md leading-relaxed rounded-2xl"
+  >
+    {desc}
+  </p>
     </div>
   );
 }
